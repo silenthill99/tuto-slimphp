4ac17514ffd8ba88abbd055d11c520fd4ae50cff
silenthill99 <florian.graziani@sfr.fr>
2025-09-16 09:28:49 +0200

    Ajout de la vérification que les infos du register ne sont pas vides

--- app/auth/register.php
+++ app/auth/register.php
@@ -3,44 +3,44 @@
 require_once "../sql/connect.php";
 /**@var PDO $db */
 if ($_POST) {
+    if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_confirmation']) &&!empty($_POST['email_confirmation']) && !empty($_POST['pseudo'])) {
+        $errors = [];
 
-    $errors = [];
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $password_confirmation = $_POST['password_confirmation'];
+        $email_confirmation = $_POST['email_confirmation'];
 
-    $pseudo = htmlspecialchars($_POST['pseudo']);
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $password_confirmation = $_POST['password_confirmation'];
-    $email_confirmation = $_POST['email_confirmation'];
+        $sql = "SELECT COUNT(pseudo) FROM users WHERE pseudo = :pseudo";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([':pseudo' => $pseudo]);
+        $count = $stmt->fetchColumn();
 
-    $sql = "SELECT COUNT(pseudo) FROM users WHERE pseudo = :pseudo";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([':pseudo' => $pseudo]);
-    $count = $stmt->fetchColumn();
+        if ($count > 0) {
+            $errors[] = "Pseudo déjà utilisé";
+            return;
+        }
 
-    if ($count > 0) {
-        $errors[] = "Pseudo déjà utilisé";
-        return;
+        if ($email != $email_confirmation) {
+            $errors[] = "Les email doivent être identiques !";
+            return;
+        }
+
+        $sql = "SELECT COUNT(email) FROM users WHERE email = :email";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([':email' => $email]);
+        $count = $stmt->fetchColumn();
+        if ($count > 0) {
+            $errors[] = "Adresse mail incorrecte";
+            return;
+        }
+
+        if ($password != $password_confirmation) {
+            $errors[] = "Les mots de passe ne correspondent pas";
+            return;
+        }
+
+        $pw_hashed = password_hash($password, PASSWORD_DEFAULT);
     }
-
-    if ($email != $email_confirmation) {
-        $errors[] = "Les email doivent être identiques !";
-        return;
-    }
-
-    $sql = "SELECT COUNT(email) FROM users WHERE email = :email";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([':email' => $email]);
-    $count = $stmt->fetchColumn();
-    if ($count > 0) {
-        $errors[] = "Adresse mail incorrecte";
-        return;
-    }
-
-    if ($password != $password_confirmation) {
-        $errors[] = "Les mots de passe ne correspondent pas";
-        return;
-    }
-
-    $pw_hashed = password_hash($password, PASSWORD_DEFAULT);
-
 }
