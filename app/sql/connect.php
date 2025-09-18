b0e393b88c1d7674a1fd684f0fb99e99ea5536bc
silenthill99 <florian.graziani@sfr.fr>
2025-09-18 11:30:36 +0200

    Ajout d'un .env.example

--- app/sql/connect.php
+++ app/sql/connect.php
@@ -1,18 +1,20 @@
 <?php
 
-try {
-    $DB_NAME = "flo";
-    $DB_HOST = "devflorian.cornillet.com";
-    $DB_PORT = 3306;
-    $DB_USER = "flo";
-    $DB_PASSWORD = "Mylene.10000";
+require_once "../../config/env.php";
 
-    $db = new PDO("mysql:host=".$DB_HOST.";port:".$DB_PORT.";dbname:".$DB_NAME, $DB_USER, $DB_PASSWORD);
+try {
+    $DB_NAME = $_ENV["DB_NAME"];
+    $DB_HOST = $_ENV["DB_HOST"];
+    $DB_PORT = $_ENV["DB_PORT"];
+    $DB_USER = $_ENV["DB_USER"];
+    $DB_PASSWORD = $_ENV["DB_PASSWORD"];
+
+    $db = new PDO("mysql:host=".$DB_HOST.";port=".$DB_PORT.";dbname=".$DB_NAME, $DB_USER, $DB_PASSWORD);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    echo "Connexion établie avec la BDD !";
+    // echo "Connexion établie avec la BDD !"; // À commenter en production
     return $db;
 } catch(PDOException $e) {
     return $e->getMessage();
