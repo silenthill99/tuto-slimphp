63a3e28a41e46a579dc4a241f9a196e9f0dc47cd
silenthill99 <florian.graziani@sfr.fr>
2025-10-23 09:39:41 +0200

    Correction d'une erreur de syntaxe

--- app/parcelle/store.php
+++ app/parcelle/store.php
@@ -5,12 +5,12 @@ session_start();
 
 require_once __DIR__ . "/../sql/connect.php";
 
-if(empty($_POST('name')) || empty($_POST('surface_in_ha')) || $_POST('surface_in_ha') == 0 || empty($_POST("description"))) exit;
+if(empty($_POST['name']) || empty($_POST['surface_in_ha']) || $_POST['surface_in_ha'] == 0 || empty($_POST["description"])) exit;
 
-$id = $_SESSION('id');
-$name = $_POST('name');
-$surface = $_POST('surface_in_ha');
-$description = $_POST('description');
+$id = $_SESSION['id'];
+$name = $_POST['name'];
+$surface = $_POST['surface_in_ha'];
+$description = $_POST['description'];
 
 /** @noinspection SqlNoDataSourceInspection */
 $sql = "INSERT INTO parcels (user_id, name, surface_in_ha) VALUES (:user_id, :name, :surface_in_ha, :description)";
