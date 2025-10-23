927ffcc790f42f063bc47c10555f778ca3cde928
silenthill99 <florian.graziani@sfr.fr>
2025-10-23 09:29:33 +0200

    Ajout de l'user id

--- app/parcelle/store.php
+++ app/parcelle/store.php
@@ -1,18 +1,21 @@
 <?php
 
 /**@var PDO $db*/
+session_start();
 
 require_once __DIR__ . "/../sql/connect.php";
 
-if(empty($_POST('name')) || empty($_POST('surface_in_ha')) || $_POST('surface_in_ha') === 0 || empty($_POST("description"))) exit;
+if(empty($_POST('name')) || empty($_POST('surface_in_ha')) || $_POST('surface_in_ha') == 0 || empty($_POST("description"))) exit;
 
+$id = $_SESSION('id');
 $name = $_POST('name');
 $surface = $_POST('surface_in_ha');
 $description = $_POST('description');
 
 /** @noinspection SqlNoDataSourceInspection */
-$sql = "INSERT INTO parcels (name, surface_in_ha) VALUES (:name, :surface_in_ha, :description)";
+$sql = "INSERT INTO parcels (user_id, name, surface_in_ha) VALUES (:user_id, :name, :surface_in_ha, :description)";
 $stmt = $db->prepare($sql);
+$stmt->bindParam(":user_id", $id);
 $stmt->bindParam(':name', $name);
 $stmt->bindParam(':surface_in_ha', $surface);
 $stmt->bindParam(':description', $description);
